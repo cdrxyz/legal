73dd0c9d495d4d5065a07ac41d643a5e07c26fee
eizus <hello@cdr.xyz>
2025-01-24 14:06:16 -0500

    Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Filling out divorce forms doesn't need to cost an arm and a leg.
+              Filling out divorce forms does not need to cost an arm and a leg.
             </h1>
             <p className="mt-6 text-lg text-gray-600">
               Costly platforms or plugins no more. Fill forms faster and cheaper.
